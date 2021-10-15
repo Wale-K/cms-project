5a6b9da0cf99377addcbfc19382fb19b090ff5d6
Wale Koiki <olawale.koiki@starlingbank.com>
2021-10-15 10:44:24 +0100

    Assest management continued.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
 
 module.exports = {
+  mode: "development", // prevents build error
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
@@ -19,6 +23,36 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
   },
