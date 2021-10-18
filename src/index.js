5112f11f433a47bec8032b9ac524a5d548a144bd
Wale Koiki <olawale.koiki@starlingbank.com>
2021-10-18 10:20:41 +0100

    Asset management continued.

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function component() {
   const element = document.createElement("div");
 
   //  Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpackkkk"], " ");
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
 
   const myIcon = new Image();
